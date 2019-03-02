0e65fee1c59e1b1cd7d17a6b751df23889206feb
Victor Lyan <wictor.lyan@gmail.com>
2019-03-02 14:47:32 +0500

    Updates

---
+++ precache-manifest.75095340c9c3070350c2a0b4db0b2920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baf581c95e77173fb80d",
+    "url": "/react-tictactoe/static/js/runtime~main.baf581c9.js"
+  },
+  {
+    "revision": "5ec674d628878ec48076",
+    "url": "/react-tictactoe/static/js/main.5ec674d6.chunk.js"
+  },
+  {
+    "revision": "ea089c583e222e215502",
+    "url": "/react-tictactoe/static/js/2.ea089c58.chunk.js"
+  },
+  {
+    "revision": "5ec674d628878ec48076",
+    "url": "/react-tictactoe/static/css/main.d111389e.chunk.css"
+  },
+  {
+    "revision": "994a5b8ebbeae7ed01d71e7f7a6769ce",
+    "url": "/react-tictactoe/index.html"
+  }
+];
